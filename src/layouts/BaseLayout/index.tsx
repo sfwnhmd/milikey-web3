a8b9e4ab83eeb03bc19509463f033c57c4b0433f
sfwnhmd <>
2023-10-18 13:09:39 +0800

    Add header component

---
+++ src/layouts/BaseLayout/index.tsx
@@ -0,0 +1,9 @@
+const BaseLayout: React.FC<{ children: React.ReactNode }> = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default BaseLayout;
